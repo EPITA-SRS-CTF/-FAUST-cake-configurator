df3f75f51c6497b128475e9ad53a438f360e2510
tim <tim>
2025-09-27 16:24:07 +0200

    fix: passwd

--- cake-configurator/db/db.sql
+++ cake-configurator/db/db.sql
@@ -23,7 +23,7 @@ REVOKE DELETE ON TABLE USERS FROM cake_user;
 REVOKE UPDATE ON TABLE USERS FROM cake_user;
 
 -- Create maintenance user
-CREATE USER maintenance_user WITH PASSWORD 'e2af446d2e8e8359218a3d09e4626782a3ea7ef8dc6b2ac26003b6dccbb47137';
+CREATE USER maintenance_user WITH PASSWORD 'cc77e3acf6aff1dbe67bd4787c8e928154296e77fab55070446123ffa6812472';
 
 -- Grant privileges
 GRANT SELECT, DELETE ON TABLE USERS TO maintenance_user;
